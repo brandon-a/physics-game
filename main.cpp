a8e5a1064faba554686ef1ab7292eb186d2f3625
Brandon Archbold <ba.brandon21@gmail.com>
2015-04-24 10:36:27 -0700

    Added Temporary Test code for SFML

--- main.cpp
+++ main.cpp
@@ -4,13 +4,31 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
+
 
 int main()
 {
-	cout << "This is the best Physics game ever!\n";
-	cout << "I AGREE!\n";
-	cout << "The game will now exit!\n";
+	std::cout << "This is the best Physics game ever!\n";
+	std::cout << "I AGREE!\n";
+	std::cout << "The game will now exit!\n";
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 	return 0;
 }
